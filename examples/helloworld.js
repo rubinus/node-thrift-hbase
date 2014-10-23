54c49bf64a7e31dec86e2e29c9b99b50f0ff734b
rubinus.chu <rubinus.chu@gmail.com>
2014-10-23 12:09:12 +0800

    rm thrift-get 完善get函数

--- examples/helloworld.js
+++ examples/helloworld.js
@@ -10,21 +10,32 @@ var config = {
 
 var hbaseClient = HBase.client(config);
 
+var get = hbaseClient.Get('domain.100');    //domain.100 is rowKey
+//get.addFamily('cf');
+get.addColumn('ip','ip');
+get.addColumn('ip','site');
+//get.addColumn('info','hobbies');
+get.setMaxVersions(1);
 
-var param = new hbaseClient.Get('TheRealMT');
-//param.addFamily('cf');
-//param.addColumn('info','name');
-//param.addColumn('info','tel');
-param.setMaxVersions(1);
-
-//console.log(param);
-
-hbaseClient.get('users',param,function(err,data){
+hbaseClient.get('logs',get,function(err,data){
     if(err){
-        console.log(err);
+        console.log('error:',err);
         return;
     }
-
     console.log(err,data);
 });
 
+
+//setTimeout(function(){
+//    var hbaseClient = HBase.client(config);
+//    var get1 = hbaseClient.Get('TheRealMT');
+//    get1.addColumn('info','hobbies');
+//
+//    hbaseClient.get('users',get1,function(err,data){
+//        if(err){
+//            console.log('error:',err);
+//            return;
+//        }
+//        console.log(err,data);
+//    });
+//},3000);
